66d1ad2dd80b03bf3fa0511a976231c30581be25
Raymond Chung <raymond_chung2@hotmail.com>
2017-07-09 20:29:38 -0400

    Updated HTML

--- js/index.js
+++ js/index.js
@@ -1,13 +1,4 @@
-$("p#sub").hide();
-$(document).ready(function() {
-	$("a#aboutBtn").click(function() {
-		if (this.innerHTML === "About") {
-			$("p#headline").hide(800);
-			$("p#sub").show(800);
-		} else {
-			$("p#sub").hide(800);
-			$("p#headline").show(800);
-		}
-		this.innerHTML = this.innerHTML === "About" ? "Home" : "About";
-	});
+AOS.init({
+	easing: 'ease-out-back',
+	duration: 900
 });
